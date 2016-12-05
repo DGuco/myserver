5e4edd09a8f8d570329996e2569581273ad6fa87
DGuco <dguco@dguco-VirtualBox.(none)>
2016-12-05 23:07:30 +0800

    ceshi

--- tcpserver/main.cpp
+++ tcpserver/main.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int main()
 {
     std::cout << "*********************" << std::endl;
-    std::cout << "*********************" << std::endl;
     std::cout << "Hello word!" << std::endl;
-    std::cout << "*********************" << std::endl;
     std::cout << "*********************" << std::endl;
 }
