ae0a81aa6ed2e37bb8f5b20299c38544c189abdd
DGuco <dguco@dguco-VirtualBox.(none)>
2016-12-05 23:04:05 +0800

    测试

--- tcpserver/main.cpp
+++ tcpserver/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main()
 {
     std::cout << "*********************" << std::endl;
+    std::cout << "*********************" << std::endl;
     std::cout << "Hello word!" << std::endl;
+    std::cout << "*********************" << std::endl;
     std::cout << "*********************" << std::endl;
 }
