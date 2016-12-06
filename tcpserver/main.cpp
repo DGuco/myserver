197f79583738710a70d6a5eac1f04db9972979b7
DGuco <1139140929@qq.com>
2016-12-06 19:11:55 +0800

    test

--- tcpserver/main.cpp
+++ tcpserver/main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main()
 {
     std::cout << "*********************" << std::endl;
+    std::cout << "*********************" << std::endl;
     std::cout << "Hello word!" << std::endl;
     std::cout << "Hello word!" << std::endl;
+    std::cout << "*********************" << std::endl;
     std::cout << "*********************" << std::endl;
 }
