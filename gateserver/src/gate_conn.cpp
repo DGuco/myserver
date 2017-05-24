da2cd9777bf143e6eebb74ab111748373556bb29
DGuco <dguco@dguco-VirtualBox.(none)>
2017-05-24 16:12:44 +0800

    gate server

--- gateserver/src/gate_conn.cpp
+++ gateserver/src/gate_conn.cpp
@@ -7,6 +7,10 @@
 
 #include "../inc/gate_conn.h"
 
+/**
+  *函数名          : EstConn
+  *功能描述        : 建立连接
+**/
 int CMyTCPConn::EstConn(int iAcceptFD)
 {
 #ifdef _POSIX_MT_
@@ -18,6 +22,10 @@ int CMyTCPConn::EstConn(int iAcceptFD)
 	return iTempRet;
 }
 
+/**
+  *函数名          : IsConnCanRecv
+  *功能描述        : 检测连接是否准备好接受数据
+**/
 int CMyTCPConn::IsConnCanRecv()
 {
 #ifdef _POSIX_MT_
@@ -32,29 +40,59 @@ int CMyTCPConn::IsConnCanRecv()
         return False;
     }
 }
-
+/**
+  *函数名          : RegToCheckSet
+  *功能描述        : 把socket添加到pCheckSet集合中
+**/
 int CMyTCPConn::RegToCheckSet(fd_set *pCheckSet) {
-	return iTempRet;
+    int iTmpRet = 0;
+#ifdef _POSIX_MT_
+    std::lock_guard<std::mutex> guard(m_stMutex);
+#endif
+    iTmpRet = GetSocket()->AddToCheckSet(pCheckSet);
+	return iTmpRet;
 }
 
+/**
+  *函数名          : IsFDSetted
+  *功能描述        : 检测socket是否添加到pCheckSet集合中
+**/
 int CMyTCPConn::IsFDSetted(fd_set *pCheckSet) {
-	int iTempRet = 0;
-
-	return iTempRet;
+#ifdef _POSIX_MT_
+    std::lock_guard<std::mutex> guard(m_stMutex);
+#endif
+    return GetSocket()->IsFDSetted(pCheckSet);
 }
 
-int CMyTCPConn::RecvAllData() {
-
-
-	return iTempRet;
+/**
+  *函数名          : RecvAllData
+  *功能描述        : 接受所有数据
+**/
+int CMyTCPConn::RecvAllData()
+{
+#ifdef _POSIX_MT_
+    std::lock_guard<std::mutex> guard(m_stMutex);
+#endif
+    return GetSocket()->RecvData();
 }
 
-int CMyTCPConn::GetOneCode(short &nCodeLength, BYTE *pCode) {
+/**
+  *函数名          : RecvAllData
+  *功能描述        : 接受指定长度的数据
+**/
+int CMyTCPConn::GetOneCode(short &nCodeLength, BYTE *pCode)
+{
+#ifdef _POSIX_MT_
+    std::lock_guard<std::mutex> guard(m_stMutex);
+#endif
 	return GetSocket()->GetOneCode((unsigned short int &)nCodeLength, pCode);
 }
 
-int CMyTCPConn::SendCode(short nCodeLength, BYTE *pCode, int iFlag /* = FLG_CONN_IGNORE */) {
-
+int CMyTCPConn::SendCode(short nCodeLength, BYTE *pCode, int iFlag /* = FLG_CONN_IGNORE */)
+{
+#ifdef _POSIX_MT_
+    std::lock_guard<std::mutex> guard(m_stMutex);
+#endif
 	return iTempRet;
 }
 
