9238b573e3a0271cc496eea78fe9ed3cebd33174
DGuco <1139140929@qq.com>
2017-08-11 19:37:54 +0800

    message 修改

--- framework/net/client_comm_engine.h
+++ framework/net/client_comm_engine.h
@@ -14,10 +14,11 @@ public:
 	static unsigned char tKey[16];
 	static unsigned char* tpKey;
 	/**
-	 * @param pBuff      client上行数据
-	 * @param unBuffLen  数据长度
-	 * @param pHead      转发给gameserver的消息C2SHead
-     * @param iTmpOffset 消息偏移
+	 * @param pBuff         client上行数据
+	 * @param unBuffLen     数据长度
+	 * @param pHead         转发给gameserver的消息C2SHead
+     * @param iTmpOffset    消息偏移
+     * @param unTmpDataLen  数据长度
 	 * @return          0：成功 1：数据不完整继续接收  其他：错误
 	 */
 	static int ParseClientStream(const void* pBuff,
@@ -25,7 +26,7 @@ public:
                                     C2SHead* pHead,
                                     unsigned int& unTmpOffset,
                                     unsigned int& unTmpDataLen);
-                      
+
     /**
      * 序列化消息发送到gameserver,如果客户端上行数据加密则解密(gateservet==>gameserver),
      * @param pBuff         存放序列化后消息的地址
@@ -70,6 +71,18 @@ public:
                                     C2SHead* pHead,
                                     Message* pMessage,
                                     CFactory* pMsgFactory = NULL);  
+
+    /**
+	 * @param pBuff         数据指针
+	 * @param unBuffLen     数据长度
+	 * @param pHead         S2CHead
+     * @param iTmpOffset    消息偏移
+	 * @return              0 成功 其他:失败错误码
+     */
+	static int ConvertStreamToMessage(const void* pBuff,
+                                    unsigned int nRecvAllLen,
+                                    S2CHead* pHead,
+                                    unsigned int& unTmpOffset);
 };
 
 #endif /* CLIENT_COMM_ENGINE_H_ */
