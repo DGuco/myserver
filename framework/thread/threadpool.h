c318a07c42a52bbb450c346b8b31858cc7f37253
DGuco <1139140929@qq.com>
2017-10-12 16:28:08 +0800

    线程修改

--- framework/thread/threadpool.h
+++ framework/thread/threadpool.h
@@ -38,30 +38,24 @@ private:
 };
 
 // the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
-        :   stop(false)
+inline ThreadPool::ThreadPool(size_t threads) :  stop(false)
 {
     for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-                [this]
+        workers.emplace_back([this]
+            {
+                for(;;)
                 {
-                    for(;;)
-                    {
-                        std::function<void()> task;
-
-                        {
-                            std::unique_lock<std::mutex> lock(this->queue_mutex);
-                            this->condition.wait(lock,
-                                                 [this]{ return this->stop || !this->tasks.empty(); });
-                            if(this->stop && this->tasks.empty())
-                                return;
-                            task = std::move(this->tasks.front());
-                            this->tasks.pop();
-                        }
-
-                        task();
-                    }
+                    std::function<void()> task;
+                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+                    this->condition.wait(lock,
+                                         [this]{ return this->stop || !this->tasks.empty(); });
+                    if(this->stop && this->tasks.empty())
+                        return;
+                    task = std::move(this->tasks.front());
+                    this->tasks.pop();
+                    task();
                 }
+            }
         );
 }
 
