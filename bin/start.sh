5a0aa5fd4b6e75e7db16c03d5b47d04b117160c7
DGuco <1139140929@qq.com>
2017-10-23 15:56:39 +0800

    脚本修改

--- bin/start.sh
+++ bin/start.sh
@@ -2,68 +2,36 @@
 user=$USER
 sh stop.sh
 
-export TRITON_HOME=/home/yaoqiang/marble
-#svn up $TRITON_HOME/dbserver
-#chmod +x $TRITON_HOME/dbserver/bin/dbsvrd
-#svn up $TRITON_HOME/proxyserver
-#chmod +x $TRITON_HOME/proxyserver/bin/proxysvrd
-#svn up $TRITON_HOME/gameserver
-#chmod +x $TRITON_HOME/gameserver/bin/gamesvrd
-#svn up $TRITON_HOME/webserver
-#chmod +x $TRITON_HOME/webserver/bin/websvrd
+export TRITON_HOME=/home/dguco/data/cpppro/myserver
+chmod +x $TRITON_HOME/bin/
 
-cd $TRITON_HOME/proxyserver/bin
-target='proxysvrd'
 rm ../log/* -f
+
+cd $TRITON_HOME/bin
+target='proxyserver'
 ./$target
 sleep 1
 
-
-cd $TRITON_HOME/dbserver/bin
-target='dbsvrd'
-rm ../log/* -f
+cd $TRITON_HOME/bin
+target='gateserver'
 ./$target
 sleep 1
 
-#cd $TRITON_HOME/checkserver/bin
-#target='checksvrd'
-#rm ../log/* -f
-#./$target
-#sleep 1
-
-#export PIPE_ID=4
-#export SC_PIPE_ID=5
-#export CS_PIPE_ID=6
-#target='worldsvrd'
-#echo $target
-#cd $TRITON_HOME/worldserver/bin
-#rm ../log/* -f
-#./$target
-#sleep 1
-
+cd $TRITON_HOME/bin
+target='dbserver'
+./$target
+sleep 1
 
 export PIPE_ID=1
 export SC_PIPE_ID=2
 export CS_PIPE_ID=3
-export SL_PIPE_ID=4
-target='gamesvrd'
+
+target='gameserver'
 echo $target
-cd $TRITON_HOME/gameserver/bin
-rm ../log/* -f
-./tcpsvrd
-sleep 1
-./logsvrd
-sleep 1
+cd $TRITON_HOME/bin
 ./$target
 sleep 1
 
-
-cd $TRITON_HOME/webserver/bin
-target='websvrd'
-rm ../log/*
-nohup ./$target &
-sleep 1
-
 ipcs -m | grep $user
-ps aux | grep $user | grep svrd | grep -v grep
+ps aux | grep $user | grep server | grep -v grep
 
