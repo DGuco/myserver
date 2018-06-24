74ef911760f132e2f89c25a66b6be7e72ee31c28
dguco <1139140929@qq.com>
2018-06-24 20:05:20 +0800

    test

--- test/main.cpp
+++ test/main.cpp
@@ -5,24 +5,67 @@
 #include <iostream>
 #include <map>
 #include <memory>
+#include <type_traits>
 using namespace std;
 
-class Test
+class Parent
 {
 public:
-    ~Test()
+    ~Parent()
     {
-        printf("~Test\n");
+        printf("~Parent\n");
+    }
+public:
+    virtual void Say()
+    {
+        printf("I am Parent\n");
     }
 };
 
+class Child: public Parent
+{
+public:
+    ~Child()
+    {
+        printf("~Child\n");
+    }
+public:
+    virtual void Say()
+    {
+        printf("I am Child\n");
+    }
+};
+
+template<typename _Ptr>
+    class Test
+    {
+    public:
+        Test(_Ptr *ptr)
+            : ptr(ptr)
+        {}
+
+        virtual ~Test()
+        {
+            delete ptr;
+        }
+    public:
+        _Ptr *ptr;
+    };
+
+void test(shared_ptr<Parent> &sharedPtr)
+{
+    printf("Use count = %d\n", sharedPtr.use_count());
+}
+
 int main()
 {
-    std::map<int, std::shared_ptr<Test>> testMap;
-    testMap.insert(std::make_pair(1, std::make_shared<Test>()));
-    testMap.insert(std::make_pair(2, std::make_shared<Test>()));
-    printf("Before Clear\n");
-    testMap.clear();
-    printf("After Clear\n");
+    Child *child = new Child;
+    Parent *parent = child;
+    parent->Say();
+    delete parent;
+//    shared_ptr<Parent> sharedPtr = std::make_shared<Child>();
+//    sharedPtr->Say();
+//    test(sharedPtr);
+//    printf("Use count = %d\n", sharedPtr.use_count());
     return 0;
 }
