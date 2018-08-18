a86b75fddf2ba81f9fd968f1eb56a6b709b3d47d
dguco <1139140929@qq.com>
2018-08-18 12:47:35 +0800

    test so动态加载

--- test/main.cpp
+++ test/main.cpp
@@ -1,139 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+#include <zconf.h>
+#include <dlfcn.h>
+#include <sys/time.h>
 //
 // Created by dguco on 18-6-18.
 //
 
-#include <iostream>
-#include <memory>
-#include <atomic>
-#include "../framework/log/log.h"
-using namespace std;
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
 
-class Parent
+void listenFileChange()
 {
-public:
-	~Parent()
-	{
-		printf("~Parent\n");
-	}
-	Parent()
-	{
-		a = 100;
-	}
+	printf("Begin listen file changed\n");
+	while (true) {
+		int length, i = 0;
+		int fd;
+		int wd;
+		char buffer[EVENT_BUF_LEN];
 
-	Parent(const Parent &tmp)
-	{
-		printf("Copy construct\n");
-	}
-public:
-	virtual void Say()
-	{
-		printf("I am Parent\n");
-	}
-	int a;
-};
+		//创建inotify实例
+		fd = inotify_init();
+		//检测是否成功
+		if (fd < 0) {
+			perror("inotify_init");
+		}
+		//监听文件
+		wd = inotify_add_watch(fd, "/data/test", IN_CREATE | IN_DELETE | IN_MODIFY);
 
-class Child: public Parent
-{
-public:
-	~Child()
-	{
-		printf("~Child\n");
-	}
+		length = read(fd, buffer, EVENT_BUF_LEN);
 
-public:
-	virtual void Say()
-	{
-		printf("I am Child\n");
-	}
-};
+		if (length < 0) {
+			perror("read");
+		}
 
-template<typename _Ptr>
-class Test
-{
-public:
-	Test(_Ptr *ptr)
-		: ptr(ptr)
-	{}
-
-	virtual ~Test()
-	{
-		delete ptr;
-	}
-public:
-	_Ptr *ptr;
-};
-
-void test(shared_ptr<Parent> &sharedPtr)
-{
-	if (sharedPtr) {
-		printf("test==>Use count = %d\n", sharedPtr.use_count());
+		while (i < length) {
+			struct inotify_event *event = (struct inotify_event *) &buffer[i];
+			if (event->len) {
+				if (event->mask & IN_CREATE) {
+					if (event->mask & IN_ISDIR) {
+						printf("New directory %s created.\n", event->name);
+					}
+					else {
+						printf("New file %s created.\n", event->name);
+					}
+				}
+				else if (event->mask & IN_DELETE) {
+					if (event->mask & IN_ISDIR) {
+						printf("Directory %s deleted.\n", event->name);
+					}
+					else {
+						printf("File %s deleted.\n", event->name);
+					}
+				}
+				else if (event->mask & IN_MODIFY) {
+					if (event->mask & IN_ISDIR) {
+						printf("Directory %s modify.\n", event->name);
+					}
+					else {
+						printf("File %s modify.\n", event->name);
+					}
+				}
+			}
+			i += EVENT_SIZE + event->len;
+		}
+		sleep(1);
+		inotify_rm_watch(fd, wd);
+		close(fd);
 	}
 }
 
-std::atomic_int atomic_int1;
-
-void add()
+// 获取当前微秒
+time_t GetUSTime()
 {
-	for (int v = 0; v < 10000; v++) {
-		int j = atomic_int1 + 2;
-		atomic_int1.store(j);
+	struct timeval tmval = {0};
+	int nRetCode = gettimeofday(&tmval, NULL);
+	if (nRetCode != 0) {
+		return 0;
 	}
+	return (long) ((tmval.tv_sec * 1000 * 1000) + tmval.tv_usec);
 }
 
-std::mutex mu;
-
-std::atomic_int atomic_int2;
-
-void add1()
+void testDlopen()
 {
-	for (int v = 0; v < 10000; v++) {
-		std::lock_guard<std::mutex> lk(mu);
-		int j = atomic_int2 + 2;
-		atomic_int2.store(j);
+	//表示要将库装载到内存，准备使用
+	long time1 = GetUSTime();
+	void *lib_handle = dlopen("/usr/local/lib/libprotoc.so.15.0.1", RTLD_NOW);
+	if (!lib_handle) {
+		fprintf(stderr, "%s\n", dlerror());
+		return;
 	}
+	long time2 = GetUSTime();
+	printf("Load Time: %ld us\n", time2 - time1);
+	time1 = GetUSTime();
+	dlclose(lib_handle);
+	time2 = GetUSTime();
+	printf("Close Time: %ld us\n", time2 - time1);
 }
-
-Parent &test1()
-{
-//	Parent *paren = new Parent;
-	Parent parent;
-	return parent;
-}
-
-#define TO_NAME(a) #a
 
 int main()
 {
-	Parent &parent = test1();
-	int a = 1;
-	std::cout << "Result: " << TO_NAME(a) << std::endl;
-	atomic_int1.store(0);
-	vector<std::thread> vector1;
-	for (int i = 0; i < 10; i++) {
-		vector1.push_back(std::move(std::thread(add)));
+	//////////////////////////////////////////////////////////////////////////////////////////
+	testDlopen();
+	void *lib_handle;
+	int (*sum)(int, int);
+	int (*sub)(int, int);
+	char *error;
+	int a = 5, b = 8;
+	//表示要将库装载到内存，准备使用
+	printf("**************libtest1so***************\n");
+	long time1 = GetUSTime();
+	lib_handle = dlopen("libtest1so.so", RTLD_LAZY);
+	if (!lib_handle) {
+		fprintf(stderr, "%s\n", dlerror());
+		return 1;
 	}
-	atomic_int2.store(0);
-	vector<std::thread> vector2;
-	for (int i = 0; i < 10; i++) {
-		vector2.push_back(std::move(std::thread(add1)));
+	long time2 = GetUSTime();
+	printf("Load Time: %ld us\n", time2 - time1);
+	///////////////////////////////////////////////////
+	//获得指定函数(symbol)在内存中的位置(指针)
+	time1 = GetUSTime();
+	sum = (int (*)(int, int)) (dlsym(lib_handle, "sum"));
+	if ((error = dlerror()) != NULL) {
+		fprintf(stderr, "%sn", error);
+		return 1;
 	}
-	std::for_each(vector1.begin(), vector1.end(), std::mem_fn(&std::thread::join));
-	std::for_each(vector2.begin(), vector2.end(), std::mem_fn(&std::thread::join));
-	printf("atomic_int1 = %d\n", atomic_int1.load());
-	printf("atomic_int2 = %d\n", atomic_int2.load());
-//	shared_ptr<Parent> sharedPtr = std::make_shared<Child>();
-//	sharedPtr->Say();
-//	printf("Use count = %d\n", sharedPtr.use_count());
-//	shared_ptr<Parent> ptr = NULL;
-//	test(ptr);
-//	test(sharedPtr);
-//	[sharedPtr]
-//	{
-//		printf("Use count = %d\n", sharedPtr.use_count());
-//		return sharedPtr.use_count();
-//	}();
-//	printf("Use count = %d\n", sharedPtr.use_count());
-//	shared_ptr<Parent> sharedPtr1 = std::move(sharedPtr);
-//	printf("Use count = %d\n", sharedPtr1.use_count());
+
+	sub = (int (*)(int, int)) (dlsym(lib_handle, "sub"));
+	if ((error = dlerror()) != NULL) {
+		fprintf(stderr, "%s\n", error);
+		return 1;
+	}
+	time2 = GetUSTime();
+	printf("Dlsym Time: %ld us\n", time2 - time1);
+	////////////////////////////////////////////////////
+
+	printf("sum:0x%x\n", sum);
+	printf("sub:0x%x\n", sub);
+	printf("a - b = %d\n", sum(a, b));
+	printf("a + b = %d\n", sub(a, b));
+	void *oldHandle = lib_handle;
+	//表示要将库装载到内存，准备使用
+	int (*sum1)(int, int);
+	int (*sub1)(int, int);
+	printf("**************libtest2so***************\n");
+	time1 = GetUSTime();
+	lib_handle = dlopen("libtest2so.so", RTLD_LAZY);
+	if (!lib_handle) {
+		fprintf(stderr, "%s\n", dlerror());
+		return 1;
+	}
+	time2 = GetUSTime();
+	printf("Load Time: %ld us\n", time2 - time1);
+	/////////////////////////////////////////////////////////
+	//获得指定函数(symbol)在内存中的位置(指针)
+	time1 = GetUSTime();
+	sum1 = (int (*)(int, int)) (dlsym(lib_handle, "sum"));
+	if ((error = dlerror()) != NULL) {
+		fprintf(stderr, "%sn", error);
+		return 1;
+	}
+
+	sub1 = (int (*)(int, int)) (dlsym(lib_handle, "sub"));
+	if ((error = dlerror()) != NULL) {
+		fprintf(stderr, "%s\n", error);
+		return 1;
+	}
+	time2 = GetUSTime();
+	printf("Dlsym Time: %ld us\n", time2 - time1);
+	//////////////////////////////////////////////////
+	dlclose(oldHandle);
+	time1 = GetUSTime();
+	sum = sum1;
+	sub = sub1;
+	time2 = GetUSTime();
+	printf("Change Time: %ld us\n", time2 - time1);
+	printf("sum:0x%x\n", sum);
+	printf("sub:0x%x\n", sub);
+	printf("a - b = %d\n", sum(a, b));
+	printf("a + b = %d\n", sub(a, b));
+	dlclose(lib_handle);
 	return 0;
 }
---
+++ testso/test.c
@@ -0,0 +1,17 @@
+//
+// Created by dguco on 18-8-18.
+//
+
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+	printf("So1 sum\n");
+	return a + b;
+}
+
+int sub(int a, int b)
+{
+	printf("So1 sub\n");
+	return a + b;
+}
