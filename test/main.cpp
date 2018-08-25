d7acd6b9b9d8ec7978179272ca16e15b9a926344
dguco <1139140929@qq.com>
2018-08-25 20:02:42 +0800

    共享内存对象内存

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,7 @@
 #include <zconf.h>
 #include <dlfcn.h>
 #include <sys/time.h>
+#include <unordered_map>
 //
 // Created by dguco on 18-6-18.
 //
@@ -21,7 +22,7 @@ void listenFileChange()
 		char buffer[EVENT_BUF_LEN];
 
 		//创建inotify实例
-		fd = inotify_init();
+		fd = inotify_init( );
 		//检测是否成功
 		if (fd < 0) {
 			perror("inotify_init");
@@ -85,24 +86,24 @@ time_t GetUSTime()
 void testDlopen()
 {
 	//表示要将库装载到内存，准备使用
-	long time1 = GetUSTime();
+	long time1 = GetUSTime( );
 	void *lib_handle = dlopen("/usr/local/lib/libprotoc.so.15.0.1", RTLD_NOW);
 	if (!lib_handle) {
-		fprintf(stderr, "%s\n", dlerror());
+		fprintf(stderr, "%s\n", dlerror( ));
 		return;
 	}
-	long time2 = GetUSTime();
+	long time2 = GetUSTime( );
 	printf("Load Time: %ld us\n", time2 - time1);
-	time1 = GetUSTime();
+	time1 = GetUSTime( );
 	dlclose(lib_handle);
-	time2 = GetUSTime();
+	time2 = GetUSTime( );
 	printf("Close Time: %ld us\n", time2 - time1);
 }
 
-int main()
+int testSoHotLoad()
 {
-	//////////////////////////////////////////////////////////////////////////////////////////
-	testDlopen();
+//////////////////////////////////////////////////////////////////////////////////////////
+	testDlopen( );
 	void *lib_handle;
 	int (*sum)(int, int);
 	int (*sub)(int, int);
@@ -110,29 +111,29 @@ int main()
 	int a = 5, b = 8;
 	//表示要将库装载到内存，准备使用
 	printf("**************libtest1so***************\n");
-	long time1 = GetUSTime();
+	long time1 = GetUSTime( );
 	lib_handle = dlopen("libtest1so.so", RTLD_LAZY);
 	if (!lib_handle) {
-		fprintf(stderr, "%s\n", dlerror());
+		fprintf(stderr, "%s\n", dlerror( ));
 		return 1;
 	}
-	long time2 = GetUSTime();
+	long time2 = GetUSTime( );
 	printf("Load Time: %ld us\n", time2 - time1);
 	///////////////////////////////////////////////////
 	//获得指定函数(symbol)在内存中的位置(指针)
-	time1 = GetUSTime();
+	time1 = GetUSTime( );
 	sum = (int (*)(int, int)) (dlsym(lib_handle, "sum"));
-	if ((error = dlerror()) != NULL) {
+	if ((error = dlerror( )) != NULL) {
 		fprintf(stderr, "%sn", error);
 		return 1;
 	}
 
 	sub = (int (*)(int, int)) (dlsym(lib_handle, "sub"));
-	if ((error = dlerror()) != NULL) {
+	if ((error = dlerror( )) != NULL) {
 		fprintf(stderr, "%s\n", error);
 		return 1;
 	}
-	time2 = GetUSTime();
+	time2 = GetUSTime( );
 	printf("Dlsym Time: %ld us\n", time2 - time1);
 	////////////////////////////////////////////////////
 
@@ -145,36 +146,36 @@ int main()
 	int (*sum1)(int, int);
 	int (*sub1)(int, int);
 	printf("**************libtest2so***************\n");
-	time1 = GetUSTime();
+	time1 = GetUSTime( );
 	lib_handle = dlopen("libtest2so.so", RTLD_LAZY);
 	if (!lib_handle) {
-		fprintf(stderr, "%s\n", dlerror());
+		fprintf(stderr, "%s\n", dlerror( ));
 		return 1;
 	}
-	time2 = GetUSTime();
+	time2 = GetUSTime( );
 	printf("Load Time: %ld us\n", time2 - time1);
 	/////////////////////////////////////////////////////////
 	//获得指定函数(symbol)在内存中的位置(指针)
-	time1 = GetUSTime();
+	time1 = GetUSTime( );
 	sum1 = (int (*)(int, int)) (dlsym(lib_handle, "sum"));
-	if ((error = dlerror()) != NULL) {
+	if ((error = dlerror( )) != NULL) {
 		fprintf(stderr, "%sn", error);
 		return 1;
 	}
 
 	sub1 = (int (*)(int, int)) (dlsym(lib_handle, "sub"));
-	if ((error = dlerror()) != NULL) {
+	if ((error = dlerror( )) != NULL) {
 		fprintf(stderr, "%s\n", error);
 		return 1;
 	}
-	time2 = GetUSTime();
+	time2 = GetUSTime( );
 	printf("Dlsym Time: %ld us\n", time2 - time1);
 	//////////////////////////////////////////////////
 	dlclose(oldHandle);
-	time1 = GetUSTime();
+	time1 = GetUSTime( );
 	sum = sum1;
 	sub = sub1;
-	time2 = GetUSTime();
+	time2 = GetUSTime( );
 	printf("Change Time: %ld us\n", time2 - time1);
 	printf("sum:0x%x\n", sum);
 	printf("sub:0x%x\n", sub);
@@ -182,4 +183,29 @@ int main()
 	printf("a + b = %d\n", sub(a, b));
 	dlclose(lib_handle);
 	return 0;
+}
+
+struct Demo
+{
+	int a;
+	int b;
+};
+
+class Test
+{
+public:
+	Test()
+	{
+		map = std::move(std::unordered_map<long, Demo>(10000));
+	}
+public:
+	std::unordered_map<long, Demo> map;
+};
+int main()
+{
+//	testDlopen();
+//	testSoHotLoad();
+//	listenFileChange();
+	Test *test = new Test;
+	printf("Test size = %d", sizeof(*test));
 }
