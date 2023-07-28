79510cea53b92715bd6590cec5acd47d00d31006
DGuco <1139140929@qq.com>
2023-07-28 15:27:57 +0800

    windows 平台兼容api开发windows 平台兼容(编译三方库的windows版)

---
+++ framework/base/platform_def.h
@@ -0,0 +1,16 @@
+/*****************************************************************
+* FileName:platform_def.h
+* Summary :
+* Date	  :2023-7-28
+* Author  :DGuco(1139140909@qq.com)
+******************************************************************/
+#ifndef __PLATFORM_DEF_H__
+#define __PLATFORM_DEF_H__
+
+#ifdef __LINUX__
+
+#else
+
+#endif
+
+#endif //__PLATFORM_DEF_H__
