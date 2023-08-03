a9a1ae0f95e63b8435af077b87df846db4013090
DGuco <1139140929@qq.com>
2023-08-03 22:28:22 +0800

    自定义数组模板和cstring模板类

--- framework/base/t_array.h
+++ framework/base/t_array.h
@@ -9,7 +9,9 @@
 
 #include "my_assert.h"
 #include <type_trais>
+#include <string>
 
+using namespace std;
 template<typename Type_,int Len_>
 class TArray
 {
@@ -30,7 +32,98 @@ class TArray
 		MY_ASSERT(index >= 0 && index < Len_, "index is out of range");
 		return m_data[index];
 	}
+
+	const Type_* GetData() const
+	{
+		return m_data;
+	}
 private:
 	Type_ m_data[Len_];
+};
+
+template<int Len_>
+class CString<Len_> : public TArray<char, Len_>
+{
+public:
+	CString()
+	{}
+
+	CString(const char* pstr)
+	{
+		if (pstr != NULL)
+		{
+			strncpy(TArray<char, Len_>::m_data, pstr, Len_);
+			TArray<char, Len_>::m_data[Len_ - 1] = '\0';
+		}
+	}
+
+	CString(std::string str)
+	{
+		if (pstr != NULL)
+		{
+			strncpy(TArray<char, Len_>::m_data, str.c_str(), Len_);
+			TArray<char, Len_>::m_data[Len_ - 1] = '\0';
+		}
+	}
+
+	template<int LenFrom_>
+	CString(CString<LenFrom_> other)
+	{
+		strncpy(TArray<char, Len_>::m_data, other.GetData(), std::min(Len_, LenFrom_));
+		TArray<char, Len_>::m_data[Len_ - 1] = '\0';
+	}
+
+	CString<Len_>& operator=(CString<Len_>& other)
+	{
+		if (this == &other)
+		{
+			return *this;
+		}
+		memset(TArray<char, Len_>::m_data, 0, sizeof(char) * Len_);
+		strncpy(TArray<char, Len_>::m_data, other.GetData(), Len_);
+		TArray<char, Len_>::m_data[Len_ - 1] = '\0';
+		return *this;
+	}
+
+	template<int LenFrom_>
+	CString<Len_>& operator=(CString<LenFrom_>& other)
+	{
+		memset(TArray<char, Len_>::m_data, 0, sizeof(char) * Len_);
+		strncpy(TArray<char, Len_>::m_data, other.GetData(), std::min(Len_, LenFrom_));
+		TArray<char, Len_>::m_data[Len_ - 1] = '\0';
+		return *this;
+	}
+
+	CString<Len_>& operator=(const char* pstr)
+	{
+		memset(TArray<char, Len_>::m_data, 0, sizeof(char) * Len_);
+		if (pstr != NULL)
+		{
+			strncpy(TArray<char, Len_>::m_data, pstr, Len_);
+			TArray<char, Len_>::m_data[Len_ - 1] = '\0';
+		}
+
+		return *this;
+	}
+	
+	template<int LenFrom_>
+	bool operator==(CString<LenFrom_>& other)
+	{
+		return std::string(TArray<char, Len_>::m_data) == std::string(other.GetData());
+	}
+
+	template<int LenFrom_>
+	bool operator==(const char* pstr)
+	{
+
+		return std::string(TArray<char, Len_>::m_data) == std::string(other.GetData());
+	}
+
+	template<int LenFrom_>
+	bool operator==(std::string str)
+	{
+
+		return std::string(TArray<char, Len_>::m_data) == str;
+	}
 };
 #endif //__T_ARRAY_H__
